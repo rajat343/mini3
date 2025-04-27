0f199cf308dba342e9498280ff27c4cd7f8ff8c6
rajat343 <rajatmishra343@gmail.com>
2025-04-26 18:30:47 -0700

    commented code removed

--- servers_start.sh
+++ servers_start.sh
@@ -1,29 +1,3 @@
-# #!/bin/bash
-# cd "$(dirname "$0")"
-
-# # Activate your venv
-# source venv/bin/activate
-
-# ports=(50051 50052 50053 50054 50055)
-# names=(Server1 Server2 Server3 Server4 Server5)
-
-# for i in "${!ports[@]}"; do
-#   port=${ports[i]}
-#   name=${names[i]}
-#   echo "🚀 Starting $name on port $port..."
-#   cmd="cd $(pwd) && source venv/bin/activate && python server.py $port $name ${ports[*]}; exec bash"
-#   osascript -e "tell application \"Terminal\" to do script \"$cmd\""
-#   sleep 1
-# done
-
-# echo "✅ All 5 servers started in new Terminal tabs."
-
-
-
-
-
-
-
 #!/bin/bash
 cd "$(dirname "$0")"
 
@@ -38,7 +12,6 @@ for i in "${!ports[@]}"; do
   name=${names[i]}
   echo "Starting $name on port $port..."
   cmd="cd $(pwd) && source venv/bin/activate && python server.py $port $name ${ports[*]}; exec bash"
-  # suppress the "tab 1 of window id ..." output
   osascript -e "tell application \"Terminal\" to do script \"$cmd\"" >/dev/null 2>&1
   sleep 1
 done
